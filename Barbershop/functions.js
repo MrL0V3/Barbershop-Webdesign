34a5b2240c96536777dbd34dd4a23f81cc06043d
unknown <kuendamayeye14@gmail.com>
2023-03-26 23:30:22 +0100

    First release of Barbershop Webdesign

---
+++ Barbershop/functions.js
@@ -0,0 +1,16 @@
+var menuBtn = document.getElementById("menu-btn")
+var sideNav = document.getElementById("side-nav")
+var menu = document.getElementById("menu")
+
+sideNav.style.right = "-250px";
+
+menu.onclick = function(){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+        menu.src = "images/close.png"
+    }else
+    {
+        sideNav.style.right = "-250px";
+        menu.src = "images/menu.png"
+    }
+}
